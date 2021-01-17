04f70e4721c982882e73e0e20b40b0c9491d4d3d
victorlomi <victorlominyo@gmail.com>
2021-01-17 10:06:17 +0300

    create base template and show index.html on homepage

--- viewer/views.py
+++ viewer/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return HttpResponse("welcome to instagram")
+    return render(request, 'index.html') 
