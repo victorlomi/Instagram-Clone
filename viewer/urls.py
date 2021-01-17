8710665c4c49c15cb1c5696a46a2adb40b1299b7
victorlomi <victorlominyo@gmail.com>
2021-01-17 15:36:46 +0300

    add signup and login view and link in index.html

--- viewer/urls.py
+++ viewer/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.homepage)        
+    path('', views.homepage, name="homepage"),
+    path('signup', views.signup, name="signup"),
+    path('login', views.login, name="login"),
 ] 
--- viewer/views.py
+++ viewer/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def homepage(request):
     return render(request, 'index.html') 
+
+
+def signup(request):
+    return HttpResponse("signup")
+
+
+def login(request):
+    return HttpResponse("login")
