f129338fd2601f4546fb077490c9364e84af83f9
victorlomi <victorlominyo@gmail.com>
2021-01-19 15:13:20 +0300

    add signup view and sign up template

--- viewer/views.py
+++ viewer/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 
 
 def homepage(request):
@@ -7,4 +9,19 @@ def homepage(request):
 
 
 def signup(request):
-    return render(request, 'registration/signup.html') 
+    if request.user.is_authenticated:
+        return redirect('homepage')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('homepage')
+        else:
+            return render(request, 'registration/signup.html', {'form': form})
+    else:
+        form = UserCreationForm()
+        return render(request, 'registration/signup.html', {'form': form})
