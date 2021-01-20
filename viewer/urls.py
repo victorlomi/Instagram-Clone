b875761a82125c5f2b516f24facbcdafafe4b27b
victorlomi <victorlominyo@gmail.com>
2021-01-20 07:14:33 +0300

    display user posts on the profile pagE

--- viewer/urls.py
+++ viewer/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views 
 
@@ -8,3 +10,6 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('profile/', views.profile, name="profile")
 ] 
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
