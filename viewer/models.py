146af2a5600ee8ac3a405bfcc2236ea441836f7c
victorlomi <victorlominyo@gmail.com>
2021-01-20 05:37:49 +0300

    create image model with one-to-many with Profile

--- viewer/models.py
+++ viewer/models.py
@@ -3,3 +3,15 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class Image(models.Model):
+	image = models.ImageField(upload_to="photos/")
+	name = models.CharField(max_length=60)
+	description = models.TextField()
+	Profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
