8a0d6c1ea5556b2141cd37ea233051b0f2be152f
victorlomi <victorlominyo@gmail.com>
2021-01-20 08:48:32 +0300

    show the images of following on timeline

--- viewer/views.py
+++ viewer/views.py
@@ -6,14 +6,23 @@ from .models import Image, Profile, Following
 
 
 def homepage(request):
-    return render(request, 'index.html') 
+    if request.user.is_authenticated:
+        current_user = Profile.objects.filter(user__username=request.user.username)[0]
+        followers = Following.objects.filter(following_profile_id=current_user.id)
+        images = []
+        for follower in followers:
+            follower_images = Image.objects.filter(profile__id=follower.profile_id.id)
+            images.append(follower_images)
+        return render(request, "index.html", {"followers": followers, "images": images})
+    else:
+        return render(request, 'index.html') 
 
 
 def profile(request):
     if request.user.is_authenticated:
         current_user = Profile.objects.filter(user__username=request.user.username)[0]
-        following = Following.objects.filter(following_profile_id=current_user.id)
-        followers = Following.objects.filter(profile_id=current_user.id)
+        followers = Following.objects.filter(following_profile_id=current_user.id)
+        following = Following.objects.filter(profile_id=current_user.id)
         posts = Image.objects.filter(profile=current_user.id)
         return render(request, "profile.html", {"posts": posts, "following": following, "followers": followers})
     else:
