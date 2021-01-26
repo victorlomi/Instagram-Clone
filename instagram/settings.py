ebb4b50250c88495654f0d8b075498814ba802e2
victorlomi <victorlominyo@gmail.com>
2021-01-26 11:34:08 +0300

    fix hosts and add localhost

--- instagram/settings.py
+++ instagram/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://instametoo.herokuapp.com']
+ALLOWED_HOSTS = ['instametoo.herokuapp.com', '127.0.0.1:5000']
 
 
 # Application definition
